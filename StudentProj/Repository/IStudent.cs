38daeddf4a88e626b7d7b53abfa344251e1a9656
Nikhil Barad <nikhilbarad465@gmail.com>
2026-05-20 10:12:24 +0530

    Implement repository pattern with asynchronous CRUD operations

---
+++ StudentProj/Repository/IStudent.cs
@@ -0,0 +1,15 @@
+﻿using StudentProj.Models;
+
+namespace StudentProj.Repository
+{
+    public interface IStudent
+    {
+        Task<List<Student>> GetAllStudentsasync();
+        Task<Student> GetStudentbyid(int id);
+        Task<int> Createstudentasync(Student student);
+        Task<bool> UpdateStudentasync(int id,Student student);
+        Task<Student> Getstudentbynameasync(string name);
+        Task<bool> DeleteStudentasync(Student student);
+
+    }
+}
---
+++ StudentProj/Repository/StudentRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentProj.Data;
+using StudentProj.Models;
+
+namespace StudentProj.Repository
+{
+    public class StudentRepository : IStudent
+    {
+        private readonly StudentDbcontext _context;
+        public StudentRepository(StudentDbcontext context) 
+        {
+            _context = context;
+        }
+        public async Task<int> Createstudentasync(Student student)
+        {
+            await _context.Student.AddAsync(student);
+            await _context.SaveChangesAsync();
+            return student.Id;
+        }
+
+        public async Task<bool> DeleteStudentasync(Student student)
+        {
+            _context.Student.Remove(student);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<List<Student>> GetAllStudentsasync()
+        {
+           return await _context.Student.ToListAsync();
+        }
+
+        public async Task<Student> GetStudentbyid(int id)
+        {
+            return await _context.Student.Where(student => student.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<Student> Getstudentbynameasync(string name)
+        {
+            return await _context.Student.Where(student => student.Name.ToLower().Equals(name.ToLower())).FirstOrDefaultAsync();
+        }
+
+        public async Task<bool> UpdateStudentasync(int id, Student student)
+        {
+            _context.Student.Update(student);
+            await _context.SaveChangesAsync();
+            return student.Id == id;
+        }
+    }
+}
