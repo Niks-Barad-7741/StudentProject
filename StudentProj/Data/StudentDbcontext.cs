17187c5dae71b01d92ca4d9524bc53e51c07d8ba
Nikhil Barad <nikhilbarad465@gmail.com>
2026-05-20 10:12:12 +0530

    Configure Entity Framework DbContext, SQL Server connection, and dependency injection

---
+++ StudentProj/Data/StudentDbcontext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentProj.Models;
+
+namespace StudentProj.Data
+{
+    public class StudentDbcontext : DbContext
+    {
+        public StudentDbcontext(DbContextOptions<StudentDbcontext> options) : base(options) 
+        {
+        }
+        public DbSet<Student> Student { get; set; }
+    }
+}
