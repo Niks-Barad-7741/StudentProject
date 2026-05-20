fed7004e55dbd943004155db9f4fb5327aa66b0c
Nikhil Barad <nikhilbarad465@gmail.com>
2026-05-20 10:12:36 +0530

    Create Entity Framework Core database migrations

---
+++ StudentProj/Migrations/20260519133744_Test2.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentProj.Migrations
+{
+    /// <inheritdoc />
+    public partial class Test2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Phone",
+                table: "Student",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "Student",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
