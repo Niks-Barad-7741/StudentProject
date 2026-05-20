a5f788393f50f2588c5cdd8048b1892d94c0693a
Nikhil Barad <nikhilbarad465@gmail.com>
2026-05-20 20:39:24 +0530

    Add authentication DTO contracts for registration and login, and extend student entity with secure password hash support

---
+++ StudentProj/DTO/LoginDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentProj.DTO
+{
+    public class LoginDTO
+    {
+
+        [Required]
+        [EmailAddress]
+        [StringLength(50)]
+
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }    
+    }
+}
--- StudentProj/DTO/RegisterDTO.cs
+++ StudentProj/DTO/RegisterDTO.cs
@@ -1,21 +1,18 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
-namespace StudentProj.Models
+namespace StudentProj.DTO
 {
-    public class Student
+    public class RegisterDTO
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }
-
         [Required]
         [StringLength(50, MinimumLength = 3)]
+
         public string Name { get; set; }
 
         [Required]
         [EmailAddress]
         [StringLength(50)]
+
         public string Email { get; set; }
 
         [Required]
@@ -28,6 +25,6 @@ namespace StudentProj.Models
         public string Phone { get; set; }
 
         [Required]
-        public string PasswordHash { get; set; }
+        public String Password { get; set; }
     }
 }
