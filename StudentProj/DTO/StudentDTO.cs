eb5683c809c99eefcc4d62d71a3ec0651d455478
Nikhil Barad <nikhilbarad465@gmail.com>
2026-05-20 10:11:57 +0530

    Create Student DTO for API request and response data transfer

---
+++ StudentProj/DTO/StudentDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentProj.DTO
+{
+    public class StudentDTO
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public int Phone { get; set; }
+    }
+}
