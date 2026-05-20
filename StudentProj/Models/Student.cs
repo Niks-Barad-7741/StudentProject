a249c597947682cb7c816f33a258be4293fa964a
Nikhil Barad <nikhilbarad465@gmail.com>
2026-05-20 10:11:32 +0530

    Create Student entity model with data annotations validation

---
+++ StudentProj/Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentProj.Models
+{
+    public class Student
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+
+        public int Phone { get; set; }
+    }
+}
