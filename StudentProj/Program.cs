dbd912aa9383e4d73a0df6d2a909e3dc87b2127f
Nikhil Barad <nikhilbarad465@gmail.com>
2026-05-20 10:07:44 +0530

    Initialize ASP.NET Core Web API project with Swagger configuration

---
+++ StudentProj/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using StudentProj.Data;
+using StudentProj.Repository;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers().AddNewtonsoftJson();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<StudentDbcontext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("StudentDb"));
+});
+builder.Services.AddScoped<IStudent, StudentRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
