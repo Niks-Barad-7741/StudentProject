01447f8e8c75339e26f4ac4ea1315f92d0b19857
Nikhil Barad <nikhilbarad465@gmail.com>
2026-05-22 16:00:44 +0530

    Added The JWT Token Generation In a Seperate file and Used Dependency Injection for  JWT Token

---
+++ StudentProj/Services/JwtService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using StudentProj.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace StudentProj.Services
+{
+    public class JwtService
+    {
+        private readonly IConfiguration _config;
+
+        public JwtService(IConfiguration config) 
+        {
+            _config = config;
+        }
+        public string GenerateToken(Student student,List<string>Roles) 
+        {
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_config["JWT-Token"])
+                );
+
+            var credintials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new List<Claim>
+            {
+                new Claim("Id",student.Id.ToString()),
+                new Claim("Name", student.Name),
+                new Claim("Email", student.Email)
+
+            };
+            foreach (var role in Roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddHours(1),
+                signingCredentials: credintials
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
