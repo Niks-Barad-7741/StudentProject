c8adca52337cc91b03fc79ddfb5e9e5b09367c57
Nikhil Barad <nikhilbarad465@gmail.com>
2026-05-28 16:47:42 +0530

    Added Logs Table To Store The Logs in the Database

---
+++ StudentProj/Models/Logs.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentProj.Models
+{
+    public class Logs
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string? Email {get; set;}
+
+        public string Action { get; set; }
+
+        public string Method { get; set; }
+
+        public string Path { get; set; }
+
+        public string IpAddress { get; set; }
+
+        public DateTime Timestamp { get; set; }
+    }
+}
