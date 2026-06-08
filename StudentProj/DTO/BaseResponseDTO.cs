56420e8a5776e413b6c5a7fea38db350cd43a4d3
Nikhil Barad <nikhilbarad465@gmail.com>
2026-06-08 11:33:24 +0530

    Untrack appsettings config files from Git and remove unused DTOs

--- StudentProj/DTO/BaseResponseDTO.cs
+++
@@ -1,8 +0,0 @@
-namespace StudentProj.DTO
-{
-    public class BaseResponseDTO
-    {
-        public int statusCodes { get; set; }
-        public string message { get; set; }
-    }
-}
--- StudentProj/DTO/FailResponseDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace StudentProj.DTO
-{
-    public class FailResponseDTO
-    {
-        public int statusCodes { get; set; }
-        public string message { get; set; }
-    }
-}
