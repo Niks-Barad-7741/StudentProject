9c9fbc5390cef5c97b4b40796519fd7e3c277e35
Nikhil Barad <nikhilbarad465@gmail.com>
2026-06-23 19:42:40 +0530

    Added Enum Message and response status code for new tables and also created the automapper of the new DTO and implemented it

--- StudentProj/Enums/ResponseStatus.cs
+++ StudentProj/Enums/ResponseStatus.cs
@@ -20,6 +20,37 @@ namespace StudentProj.Enums
         PermissionRevokedSuccessfully,
         RoleRetriveSuccessfully,
         PermissionRetriveSuccessfully,
+        CourseNotFound,
+        CourseCreatedSuccessfully,
+        CourseUpdatedSuccessfully,
+        CourseRetriveSuccessfully,
+        CourseSoftDeletedSuccessfully,
+        EnrollmentNotFound,
+        EnrollmentAddedSuccessfully,
+        EnrollmentRetriveSuccessfully,
+        EnrollmentDeletedSuccessfully,
+        EnrollmentUpdatedSuccessfully,
+        StudentAlreadyEnrolled,
+        GradeUpdatedSuccessfully,
+        GradeUpdateFailed,
+        StudentNotEnrolled,
+        InvalidGradeValue,
+        EnrollmentSoftDeletedSuccessfully,
+        EnrollmentRestoredSuccessfully,
+        AttendanceNotFound,
+        AttendanceAddedSuccessfully,
+        AttendanceRetriveSuccessfully,
+        AttendanceDeletedSuccessfully,
+        SubjectNotFound,
+        SubjectAddedSuccessfully,
+        SubjectUpdatedSuccessfully,
+        SubjectRetriveSuccessfully,
+        SubjectSoftDeletedSuccessfully,
+        SubjectRestoredSuccessfully,
+        SubjectAlreadyExists,
+        SubjectNotBelongsToCourse,
+        SubjectAlreadyEnrolled,
+        SubjectNotEnrolled,
 
         // Failure/Validation Operations
         UserAlreadyExist,
@@ -33,7 +64,8 @@ namespace StudentProj.Enums
         Unauthorized,
         Forbidden,
         NotFound,
-        InternalServerError
+        InternalServerError,
+        InvalidData
     }
 
     public static class ResponseStatusExtensions
@@ -68,6 +100,46 @@ namespace StudentProj.Enums
             ResponseStatus.Forbidden => 403,
             ResponseStatus.NotFound => 404,
             ResponseStatus.InternalServerError => 500,
+
+
+            ResponseStatus.CourseNotFound => 404,
+            ResponseStatus.CourseCreatedSuccessfully => 201,
+            ResponseStatus.CourseUpdatedSuccessfully => 200,
+            ResponseStatus.CourseRetriveSuccessfully => 200,
+            ResponseStatus.CourseSoftDeletedSuccessfully => 200,
+            
+            
+            ResponseStatus.EnrollmentNotFound => 404,
+            ResponseStatus.EnrollmentAddedSuccessfully => 201,
+            ResponseStatus.EnrollmentRetriveSuccessfully=> 200,
+            ResponseStatus.EnrollmentDeletedSuccessfully => 200,
+            ResponseStatus.EnrollmentUpdatedSuccessfully => 200,
+            ResponseStatus.StudentAlreadyEnrolled => 409,
+            ResponseStatus.GradeUpdatedSuccessfully => 200,
+            ResponseStatus.GradeUpdateFailed => 400,
+            ResponseStatus.StudentNotEnrolled => 404,
+            ResponseStatus.InvalidGradeValue => 400,
+            ResponseStatus.EnrollmentSoftDeletedSuccessfully => 200,
+            ResponseStatus.EnrollmentRestoredSuccessfully => 200,
+
+            ResponseStatus.AttendanceNotFound => 404,
+            ResponseStatus.AttendanceAddedSuccessfully => 201,
+            ResponseStatus.AttendanceRetriveSuccessfully => 200,
+            ResponseStatus.AttendanceDeletedSuccessfully => 200,
+
+
+            ResponseStatus.SubjectNotFound => 404,
+            ResponseStatus.SubjectAddedSuccessfully => 201,
+            ResponseStatus.SubjectUpdatedSuccessfully => 200,
+            ResponseStatus.SubjectRetriveSuccessfully => 200,
+            ResponseStatus.SubjectSoftDeletedSuccessfully => 200,
+            ResponseStatus.SubjectRestoredSuccessfully => 200,
+            ResponseStatus.SubjectAlreadyExists => 409,
+            ResponseStatus.SubjectNotBelongsToCourse => 400,
+            ResponseStatus.SubjectAlreadyEnrolled => 409,
+            ResponseStatus.SubjectNotEnrolled => 404,
+
+            ResponseStatus.InvalidData => 400,
             _ => 200
         };
 
@@ -96,11 +168,52 @@ namespace StudentProj.Enums
             ResponseStatus.RoleNotFound => ApiMessages.RoleNotFound,
             ResponseStatus.DefaultRoleNotFound => ApiMessages.DefaultRoleNotFound,
             ResponseStatus.PermissionNotFound => ApiMessages.PermissionNotFound,
+            ResponseStatus.InvalidData => "Invalid data provided. Please check your input and try again.",
             ResponseStatus.BadRequest => "Bad Request",
             ResponseStatus.Unauthorized => "Unauthorized. Please log in.",
             ResponseStatus.Forbidden => "Forbidden. You do not have the required permission.",
             ResponseStatus.NotFound => "Not Found",
             ResponseStatus.InternalServerError => "An unexpected error occurred.",
+
+
+            ResponseStatus.CourseNotFound => ApiMessages.CourseNotFound,
+            ResponseStatus.CourseCreatedSuccessfully => ApiMessages.CourseCreatedSuccessfully,
+            ResponseStatus.CourseUpdatedSuccessfully => ApiMessages.CourseUpdatedSuccessfully,
+            ResponseStatus.CourseRetriveSuccessfully => ApiMessages.CourseRetriveSuccessfully,
+            ResponseStatus.CourseSoftDeletedSuccessfully => ApiMessages.CourseSoftDeletedSuccessfully,
+
+
+            ResponseStatus.EnrollmentNotFound => ApiMessages.EnrollmentNotFound,
+            ResponseStatus.EnrollmentAddedSuccessfully => ApiMessages.EnrollmentAddedSuccessfully,
+            ResponseStatus.EnrollmentRetriveSuccessfully => ApiMessages.EnrollmentRetriveSuccessfully,
+            ResponseStatus.EnrollmentDeletedSuccessfully => ApiMessages.EnrollmentDeletedSuccessfully,
+            ResponseStatus.EnrollmentUpdatedSuccessfully => ApiMessages.EnrollmentUpdatedSuccessfully,
+            ResponseStatus.StudentAlreadyEnrolled => ApiMessages.StudentAlreadyEnrolled,
+            ResponseStatus.GradeUpdatedSuccessfully => ApiMessages.GradeUpdatedSuccessfully,
+            ResponseStatus.GradeUpdateFailed => ApiMessages.GradeUpdateFailed,
+            ResponseStatus.StudentNotEnrolled => ApiMessages.StudentNotEnrolled,
+            ResponseStatus.InvalidGradeValue => ApiMessages.InvalidGradeValue,
+            ResponseStatus.EnrollmentSoftDeletedSuccessfully => ApiMessages.EnrollmentSoftDeletedSuccessfully,
+            ResponseStatus.EnrollmentRestoredSuccessfully => ApiMessages.EnrollmentRestoredSuccessfully,
+
+
+            ResponseStatus.AttendanceNotFound => ApiMessages.AttendanceNotFound,
+            ResponseStatus.AttendanceAddedSuccessfully => ApiMessages.AttendanceAddedSuccessfully,
+            ResponseStatus.AttendanceRetriveSuccessfully => ApiMessages.AttendanceRetriveSuccessfully,
+            ResponseStatus.AttendanceDeletedSuccessfully => ApiMessages.AttendanceDeletedSuccessfully,
+
+
+            ResponseStatus.SubjectNotFound => ApiMessages.SubjectNotFound,
+            ResponseStatus.SubjectAddedSuccessfully => ApiMessages.SubjectAddedSuccessfully,
+            ResponseStatus.SubjectUpdatedSuccessfully => ApiMessages.SubjectUpdatedSuccessfully,
+            ResponseStatus.SubjectRetriveSuccessfully => ApiMessages.SubjectRetriveSuccessfully,
+            ResponseStatus.SubjectSoftDeletedSuccessfully => ApiMessages.SubjectSoftDeletedSuccessfully,
+            ResponseStatus.SubjectRestoredSuccessfully => ApiMessages.SubjectRestoredSuccessfully,
+            ResponseStatus.SubjectAlreadyExists => ApiMessages.SubjectAlreadyExists,
+            ResponseStatus.SubjectNotBelongsToCourse => ApiMessages.SubjectNotBelongsToCourse,
+            ResponseStatus.SubjectAlreadyEnrolled => ApiMessages.SubjectAlreadyEnrolled,
+            ResponseStatus.SubjectNotEnrolled => ApiMessages.SubjectNotEnrolled,
+
             _ => status.ToString()
         };
 
--- StudentProj/Mapping/MappingProfile.cs
+++ StudentProj/Mapping/MappingProfile.cs
@@ -24,6 +24,30 @@ namespace StudentProj.Mapping
 
             // Route Permission mappings
             CreateMap<RoutePermissions, RoutePermissionDTO>().ReverseMap();
+
+            //Course Mapping
+            CreateMap<Course, CourseDTO>().ReverseMap();
+            CreateMap<CreateCourseDTO, Course>();
+            CreateMap<UpdateCourseDTO, Course>();
+
+            //Subject Mapping
+            CreateMap<Subject, SubjectDTO>().ReverseMap();
+            CreateMap<CreateSubjectDTO, Subject>();
+
+            //Enrollment Mapping
+            CreateMap<EnrollStudentDTO, Enrollment>();
+            CreateMap<Enrollment, EnrollmentDTO>()
+                .ForMember(dest => dest.StudentName, opt => opt.MapFrom(src => src.Student.Name))
+                .ForMember(dest => dest.CourseName, opt => opt.MapFrom(src => src.Course.CourseName));
+
+            //Attendance Mapping
+            CreateMap<RecordAttendanceDTO, Attendance>();
+            CreateMap<Attendance, AttendanceDTO>()
+                .ForMember(dest => dest.StudentName, opt => opt.MapFrom(src => src.Student.Name))
+                .ForMember(dest => dest.SubjectName, opt => opt.MapFrom(src => src.Subject.SubjectName));
+
+            //Logs Mapping
+            CreateMap<Logs,LogResponseDTO>();
         }
     }
 }
